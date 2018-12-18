54f2a14b2be5af32c13620298d1203b50ecce8cc
KessaPassa <k.someya.gyao@gmail.com>
2018-12-18 23:27:16 +0900

    [fix] ディレクトリを整理+Scenargie専用に変更

--- distribute_people_2D.py
+++ distribute_people_2D.py
@@ -18,15 +18,15 @@ def get_write_path():
 
 
 # エリアと時間別に人数を+1ずつしていく
-def distribute_people(base, main):
+def distribute_people(base, read):
     """
     :type base: pd.DataFrame
-    :type main: pd.DataFrame
+    :type read: pd.DataFrame
     """
     df_new = pd.DataFrame()
 
     # グルーピングすることでforが回る数を減らし高速化
-    group_list = main.groupby(['time'])
+    group_list = read.groupby(['time'])
     for _name, _group in group_list:
         # 同じ時間帯のみコピーで取り出す
         tmp = base.loc[base['time'] == _name].copy()
@@ -66,14 +66,14 @@ if __name__ == '__main__':
     for _dir in dir_list:
         for _seed in seed_list:
             for _csv in csv_list:
-                main_csv = pd.read_csv(get_read_path() + _dir + 'seed' + _seed + '_' + _csv + '.csv',
-                                       encoding='Shift_JISx0213',
-                                       dtype=None,
-                                       delimiter=',')
+                df_read = pd.read_csv(get_read_path() + _dir + 'seed' + _seed + '_' + _csv + '.csv',
+                                      encoding='Shift_JISx0213',
+                                      dtype=None,
+                                      delimiter=',')
 
                 start = time.time()
 
-                output = distribute_people(df_base.copy(), main_csv)
+                output = distribute_people(df_base.copy(), df_read)
                 output.to_csv(get_write_path() + _dir + 'seed' + _seed + '_' + _csv + '.csv')
                 print(_dir + 'seed' + _seed + _csv + '.csv')
 
