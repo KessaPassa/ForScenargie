8e6e49348ab294164b090f8124705cf70e608ed8
KessaPassa <k.someya.gyao@gmail.com>
2019-01-04 14:13:34 +0900

    [fix] interpolated_distribute_3d.pyからindex_col=0を廃止

--- distribute_people_3d.py
+++ distribute_people_3d.py
@@ -19,9 +19,9 @@ def get_write_path():
 # エリアと時間別に人数を+1ずつしていく
 def distribute_people(base, read):
     """
-        :type base: pd.DataFrame
-        :type read: pd.DataFrame
-        """
+    :type base: pd.DataFrame
+    :type read: pd.DataFrame
+    """
     df_new = pd.DataFrame()
 
     # グルーピングすることでforが回る数を減らし高速化
@@ -53,9 +53,7 @@ if __name__ == '__main__':
     for _dir in dir_list:
         for _seed in seed_list:
             for _csv in csv_list:
-                df_read = pd.read_csv(get_read_path() + _dir + 'seed' + _seed + '_' + _csv + '.csv', index_col=0)
+                df_read = pd.read_csv(get_read_path() + _dir + 'seed' + _seed + '_' + _csv + '.csv')
                 output = distribute_people(df_base.copy(), df_read)
                 output.to_csv(get_write_path() + _dir + 'seed' + _seed + '_' + _csv + '.csv')
                 print(_dir + 'seed' + _seed + _csv + '.csv')
-
-
